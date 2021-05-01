c4538290d018bb33dfad08a6cec4406b00bf5f43
JohnAL95 <joao.afonso.1995@gmail.com>
2021-05-01 10:28:14 +0200

    test(participantitem): added tests for the participant item component

--- client/src/components/Dashboard/ListParticipants/ParticipantItem/ParticipantItem.test.js
+++ client/src/components/Dashboard/ListParticipants/ParticipantItem/ParticipantItem.test.js
@@ -10,40 +10,64 @@ import { StaticRouter } from "react-router-dom";
 import ParticipantItem from "./ParticipantItem";
 
 
+jest.mock("@/components", () => ({
+  DeleteParticipantButton: () => <div data-testid="delete-participant-button"></div>,
+  StatusLight: () => <div data-testid="status-light"></div>,
+}));
+
+
 let participant;
 let promptPopup;
 
 beforeEach(() => {
   participant = {
-    id: 1,
+    id: 5,
     first_name: "John",
     last_name: "Gallard",
     registration_status: "New",
     instrument: {
+      id: 1,
       name: "Fiddle",
     },
-    is_underage: true,
     email: "email@gmail.com",
+    is_underage: true,
+    displayed: true,
   }
   promptPopup = '';
 });
 
-it("ParticipantItemの一つ目のテスト", async () => {
-  // arrange
-  await act(async () => {
-    render(
-      <StaticRouter>
-        <ParticipantItem participant={participant} promptPopup={promptPopup} />
-      </StaticRouter>
-    );
-  })
+describe("ParticipantItem is rendering properly", () => {
 
-  // act
+  it("should display the participant details on the page", async () => {
+    await act(async () => {
+      render(
+        <StaticRouter>
+          <ParticipantItem participant={participant} promptPopup={promptPopup} />
+        </StaticRouter>
+      );
+    });
 
+    expect(screen.queryByText(/John/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Gallard/i)).toBeInTheDocument();
+    expect(screen.queryByText(/email@gmail\.com/i)).toBeInTheDocument();
+    expect(screen.queryByText(/fiddle/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Mark/)).not.toBeInTheDocument();
+  });
 
-  // assert
-  expect(screen.queryByText(/John/)).toBeInTheDocument();
-  expect(screen.queryByText(/Gallard/)).toBeInTheDocument();
-  expect(screen.queryByText(/email@gmail\.com/)).toBeInTheDocument();
-  expect(screen.queryByText(/Mark/)).not.toBeInTheDocument();
+  it("should display a status light and a delete button", async () => {
+    await act(async () => {
+      render(
+        <StaticRouter>
+          <ParticipantItem participant={participant} promptPopup={promptPopup} />
+        </StaticRouter>
+      );
+    });
+
+    expect(screen.queryByTestId("delete-participant-button"))
+      .toBeInTheDocument();
+    expect(screen.queryByTestId("status-light"))
+      .toBeInTheDocument();
+  });
+
 });
+
